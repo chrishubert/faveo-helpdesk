a1f9f3a8e4da0779b05751239a15f67274b63058
Christophe Hubert <hubert.christophe@gmail.com>
2021-05-21 17:37:06 +0700

    Fix Read Notification

--- app/Http/ViewComposers/AgentLayout.php
+++ app/Http/ViewComposers/AgentLayout.php
@@ -50,19 +50,19 @@ class AgentLayout
      */
     public function compose(View $view)
     {
-        $notifications = \App\Http\Controllers\Common\NotificationController::getNotifications();
+        $notifications = \App\Http\Controllers\Common\NotificationController::getNotifications()->where('is_unread', '=', 0);
         $view->with([
-            'company'            => $this->company,
-            'notifications'      => $notifications,
-            'myticket'           => $this->myTicket(),
-            'unassigned'         => $this->unassigned(),
-            'followup_ticket'    => $this->followupTicket(),
-            'deleted'            => $this->deleted(),
-            'tickets'            => $this->inbox(),
-            'department'         => $this->departments(),
-            'overdues'           => $this->overdues(),
-            'due_today'          => $this->getDueToday(),
-            'is_mail_conigured'  => $this->getEmailConfig(),
+            'company' => $this->company,
+            'notifications' => $notifications,
+            'myticket' => $this->myTicket(),
+            'unassigned' => $this->unassigned(),
+            'followup_ticket' => $this->followupTicket(),
+            'deleted' => $this->deleted(),
+            'tickets' => $this->inbox(),
+            'department' => $this->departments(),
+            'overdues' => $this->overdues(),
+            'due_today' => $this->getDueToday(),
+            'is_mail_conigured' => $this->getEmailConfig(),
             'dummy_installation' => $this->getDummyDataInstallation(),
         ]);
     }
@@ -85,11 +85,11 @@ class AgentLayout
             $tickets = $tickets->where('tickets.dept_id', '=', \Auth::user()->primary_dpt);
         }
         $tickets = $tickets
-                ->leftJoin('department as dep', 'tickets.dept_id', '=', 'dep.id')
-                ->leftJoin('ticket_status', 'tickets.status', '=', 'ticket_status.id')
-                ->select('dep.name as name', 'ticket_status.name as status', \DB::raw('COUNT(ticket_status.name) as count'))
-                ->groupBy('dep.name', 'ticket_status.name')
-                ->get();
+            ->leftJoin('department as dep', 'tickets.dept_id', '=', 'dep.id')
+            ->leftJoin('ticket_status', 'tickets.status', '=', 'ticket_status.id')
+            ->select('dep.name as name', 'ticket_status.name as status', \DB::raw('COUNT(ticket_status.name) as count'))
+            ->groupBy('dep.name', 'ticket_status.name')
+            ->get();
         $grouped = $tickets->groupBy('name');
         $status = [];
         foreach ($grouped as $key => $group) {
@@ -104,10 +104,10 @@ class AgentLayout
         $ticket = $this->tickets();
         if ($this->auth->role == 'admin') {
             return $ticket->where('assigned_to', $this->auth->id)
-                    ->where('status', '1');
+                ->where('status', '1');
         } elseif ($this->auth->role == 'agent') {
             return $ticket->where('assigned_to', $this->auth->id)
-                    ->where('status', '1');
+                ->where('status', '1');
         }
     }
 
@@ -116,13 +116,13 @@ class AgentLayout
         $ticket = $this->tickets();
         if ($this->auth->role == 'admin') {
             return $ticket->where('assigned_to', '=', null)
-                    ->where('status', '=', '1')
-                    ->select('id');
+                ->where('status', '=', '1')
+                ->select('id');
         } elseif ($this->auth->role == 'agent') {
             return $ticket->where('assigned_to', '=', null)
-                    ->where('status', '=', '1')
-                    ->where('dept_id', '=', $this->auth->primary_dpt)
-                    ->select('id');
+                ->where('status', '=', '1')
+                ->where('dept_id', '=', $this->auth->primary_dpt)
+                ->select('id');
         }
     }
 
@@ -143,7 +143,7 @@ class AgentLayout
             return $ticket->where('status', '5')->select('id');
         } elseif ($this->auth->role == 'agent') {
             return $ticket->where('status', '5')->where('dept_id', '=', $this->auth->primary_dpt)
-                    ->select('id');
+                ->select('id');
         }
     }
 
@@ -157,7 +157,7 @@ class AgentLayout
 
         return $table->Join('ticket_status', function ($join) {
             $join->on('ticket_status.id', '=', 'tickets.status')
-                        ->whereIn('ticket_status.id', [1, 7]);
+                ->whereIn('ticket_status.id', [1, 7]);
         });
     }
 
@@ -166,19 +166,19 @@ class AgentLayout
         $ticket = $this->tickets();
         if ($this->auth->role == 'admin') {
             return $ticket->where('status', '=', 1)
-                            ->where('isanswered', '=', 0)
-                            ->whereNotNull('tickets.duedate')
-                            ->where('tickets.duedate', '!=', '00-00-00 00:00:00')
-                            ->where('tickets.duedate', '<', \Carbon\Carbon::now())
-                            ->select('tickets.id');
+                ->where('isanswered', '=', 0)
+                ->whereNotNull('tickets.duedate')
+                ->where('tickets.duedate', '!=', '00-00-00 00:00:00')
+                ->where('tickets.duedate', '<', \Carbon\Carbon::now())
+                ->select('tickets.id');
         } elseif ($this->auth->role == 'agent') {
             return $ticket->where('status', '=', 1)
-                            ->where('isanswered', '=', 0)
-                            ->whereNotNull('tickets.duedate')
-                            ->where('dept_id', '=', $this->auth->primary_dpt)
-                            ->where('tickets.duedate', '!=', '00-00-00 00:00:00')
-                            ->where('tickets.duedate', '<', \Carbon\Carbon::now())
-                            ->select('tickets.id');
+                ->where('isanswered', '=', 0)
+                ->whereNotNull('tickets.duedate')
+                ->where('dept_id', '=', $this->auth->primary_dpt)
+                ->where('tickets.duedate', '!=', '00-00-00 00:00:00')
+                ->where('tickets.duedate', '<', \Carbon\Carbon::now())
+                ->select('tickets.id');
         }
     }
 
@@ -187,26 +187,26 @@ class AgentLayout
         $ticket = $this->tickets();
         if ($this->auth->role == 'admin') {
             return $ticket->where('status', '=', 1)
-                            ->where('status', '=', 1)
-                            ->where('isanswered', '=', 0)
-                            ->whereNotNull('duedate')
-                            ->whereRaw('date(duedate) = ?', [date('Y-m-d')]);
+                ->where('status', '=', 1)
+                ->where('isanswered', '=', 0)
+                ->whereNotNull('duedate')
+                ->whereRaw('date(duedate) = ?', [date('Y-m-d')]);
         } elseif ($this->auth->role == 'agent') {
             return $ticket->where('status', '=', 1)
-                            ->where('status', '=', 1)
-                            ->where('isanswered', '=', 0)
-                            ->whereNotNull('duedate')
-                            ->where('dept_id', '=', $this->auth->primary_dpt)
-                            ->whereRaw('date(duedate) = ?', [date('Y-m-d')]);
+                ->where('status', '=', 1)
+                ->where('isanswered', '=', 0)
+                ->whereNotNull('duedate')
+                ->where('dept_id', '=', $this->auth->primary_dpt)
+                ->whereRaw('date(duedate) = ?', [date('Y-m-d')]);
         }
     }
 
     /**
-     * @category function to check configured mails
-     *
+     * @return bool true/false
      * @var
      *
-     * @return bool true/false
+     * @category function to check configured mails
+     *
      */
     public function getEmailConfig()
     {
@@ -219,11 +219,11 @@ class AgentLayout
     }
 
     /**
-     * @category function to check if dummy data is installed in the system or not
-     *
      * @param null
      *
      * @return builder
+     * @category function to check if dummy data is installed in the system or not
+     *
      */
     public function getDummyDataInstallation()
     {
